e0f450a07572870c80955f960321e6503621235d
harishjartarghar <harish.jartarghar@gmail.com>
2020-05-01 12:28:27 +0530

    MenuComponent part 1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import { Navbar , NavbarBrand } from 'reactstrap';
 import './App.css';
+import Menu from "./components/MenuComponent";
 
 function App() {
   return (
@@ -11,7 +12,9 @@ function App() {
         <NavbarBrand href="#">HARISH</NavbarBrand>
       </div>
      </Navbar>
+     <Menu/>
     </div>
+    
   );
 }
 
