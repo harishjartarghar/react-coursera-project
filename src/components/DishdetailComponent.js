389068eccf7a75adcfe921f533442da5df2017b9
harishjartarghar <harish.jartarghar@gmail.com>
2020-05-15 20:50:45 +0530

    assignment 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,17 +1,34 @@
 import React from "react";
-import { Card,CardBody,CardText,CardImg, CardTitle } from "reactstrap";
-
-
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 const Dishdetail=(props)=>{
 
     
     
     const {dish} = props;
-    return ( 
-        <div >
+    return (
+        <div className="container">
+        <div className="row">
+            <Breadcrumb>
 
-        <RenderDish dish={props.dish}/>
-
+                <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+            </Breadcrumb>
+            <div className="col-12">
+                <h3>{props.dish.name}</h3>
+                <hr />
+            </div>                
+        </div>
+        <div className="row">
+            <div className="col-12 col-md-5 m-1">
+                <RenderDish dish={props.dish} />
+            </div>
+            <div className="col-12 col-md-5 m-1">
+            <h4>Comments</h4>
+                <RenderComments comments={props.comments} />
+            </div>
+        </div>
         </div>
     );
 }
@@ -22,21 +39,13 @@ const Dishdetail=(props)=>{
         if (dish!=null)
         {
             return(
-            <div className="row">
-            <div className="col-12 col-md-5 m-1">
-            <Card>
-                <CardImg width="100%" src={dish.image} alt={dish.name} />
-                <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                </CardBody>
-            </Card>
-            </div>
-            <div className="col-12 col-md-5 m-1" >
-            <h4>Comments</h4>
-            <RenderComments comments={dish.comments}/>
-            </div>
-            </div>
+                    <Card>
+                     <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                         </CardBody>
+                     </Card>
             )
         }
         else{
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 import { Navbar , NavbarBrand } from 'reactstrap';
 import Menu from "./MenuComponent";
 import Dishdetail from "./DishdetailComponent";
-import {DISHES} from '../shared/dishes'
+import {DISHES} from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import Header from './HeaderComponents';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
+import About from './AboutComponent';
 import {Switch, Route, Redirect} from 'react-router-dom';
 
 class Main extends Component {
@@ -13,10 +18,12 @@ class Main extends Component {
 
   constructor(props){
     super(props);
-    this.state={
-      dishes:DISHES,
-      SelectedDish:null
-    }
+    this.state = {
+      dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
+    };
   }
 
     onDishSelect=(dishID)=>
@@ -24,21 +31,38 @@ class Main extends Component {
         this.setState({selectedDish:dishID});
     }
 
+   
+    
+
 
   render()
   {
+    const DishWithId = ({match}) => {
+      return(
+          <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
 
     const HomePage=()=>{
       return (
-        <Home/>
+        <Home 
+        dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+        promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+        leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+    />
       );
     }
+
     return (
           <React.Fragment>
             <Header/>
            <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Route path='/menu/:dishId' component={DishWithId} />
+              <Route exact path='/contactus' component={Contact} />} />
+              <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
               <Redirect to="/home" />
            </Switch>
           <Footer/>
